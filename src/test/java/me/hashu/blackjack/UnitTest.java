e4a3a78d47f0abacc16bbd6e6be85fa6957264c9
hazashbaig <hazashbaig@gmail.com>
2021-08-19 17:07:28 +0530

    Add UnitTest to check getSum() method of Hand class

---
+++ src/test/java/me/hashu/blackjack/UnitTest.java
@@ -0,0 +1,24 @@
+package me.hashu.blackjack;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class UnitTest {
+
+    @Test
+    @DisplayName("check the getSum() method of Hand class")
+    void checkHandSum() {
+        Hand hand = new Hand();
+        hand.addCard(new Card(Rank.QUEEN, Suit.HEART));
+        hand.addCard(new Card(Rank.TEN, Suit.SPADE));
+        hand.addCard(new Card(Rank.ACE, Suit.DIAMOND));
+        hand.addCard(new Card(Rank.SEVEN, Suit.CLUB));
+
+        assertThat(hand.getSum()).isEqualTo(28);
+    }
+
+}
