1f368382f1c3eedab6b1cb0cdcf051921b41232f
hazashbaig <hazashbaig@gmail.com>
2021-08-19 16:30:10 +0530

    Add Hand Class that uses list of cards

---
+++ src/main/java/me/hashu/blackjack/Hand.java
@@ -0,0 +1,19 @@
+package me.hashu.blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    private List<Card> cards = new ArrayList<>();
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int getSum() {
+        return cards.stream()
+                .map(Card::getRank)
+                .map(Rank::getValue)
+                .reduce(0, Integer::sum);
+    }
+}
