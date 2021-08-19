055fc6183e66fd6e5b5cf3ca25a3fa4a8e6641e5
hazashbaig <hazashbaig@gmail.com>
2021-08-19 17:55:43 +0530

    add test profile

--- src/test/java/me/hashu/blackjack/UnitTest.java
+++ src/test/java/me/hashu/blackjack/UnitTest.java
@@ -3,9 +3,11 @@ package me.hashu.blackjack;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class UnitTest {
 
