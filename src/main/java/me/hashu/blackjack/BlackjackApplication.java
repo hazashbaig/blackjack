395c6ba4efcded927734773b5245de6940cde14a
hazashbaig <hazashbaig@gmail.com>
2021-08-19 12:46:33 +0530

    Intialized spring boot project

---
+++ src/main/java/me/hashu/blackjack/BlackjackApplication.java
@@ -0,0 +1,13 @@
+package me.hashu.blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackApplication.class, args);
+	}
+
+}
