b7f7caa9e323bb21ebc2bbbd6adf746d11c90b01
hazashbaig <hazashbaig@gmail.com>
2021-08-19 16:14:18 +0530

    Create Card class that uses Rank and Suit enums

---
+++ src/main/java/me/hashu/blackjack/Card.java
@@ -0,0 +1,19 @@
+package me.hashu.blackjack;
+
+public class Card {
+    private final Rank rank;
+    private final Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+}
